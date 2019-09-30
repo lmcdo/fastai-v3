746954b04c2f5b1d1bc79bdd095409684dd0878a
Nasir Sattar <lawrence.mcdonell@gmail.com>
2019-09-30 17:20:42 +1000

    fix merge

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1WVelP0OZ8ZcSv4PM2O9yPyvn5MYHpcSr'
+export_file_url = 'https://drive.google.com/open?id=1Ba7RUkzWf7IrbxJFaxpa6xb6cI99cSx6'
 export_file_name = 'export.pkl'
 
 classes = ['red-necked', 'tammar', 'agile']
